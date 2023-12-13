182743f44cbfc9881797dbc2acde3ac7969bcb5a
Bartłomiej Krukowski <bartekkrukowski822@gmail.com>
2023-12-13 12:45:34 +0100

    added a class responsible for changing the view

--- src/main/java/com/example/mailmanagementsystem/controllers/LoginController.java
+++ src/main/java/com/example/mailmanagementsystem/controllers/LoginController.java
@@ -1,29 +1,31 @@
 package com.example.mailmanagementsystem.controllers;
 
+import com.example.mailmanagementsystem.utils.ViewSwitcher;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.*;
-import javafx.scene.layout.FlowPane;
-import javafx.scene.layout.HBox;
+import javafx.scene.control.RadioButton;
 
 import java.io.IOException;
 
 public class LoginController {
-
+    @FXML
+    private RadioButton radioButtonClient;
+    @FXML
+    private RadioButton radioButtonEmployee;
+    @FXML
+    private RadioButton radioButtonAdmin;
 
     public void login(ActionEvent event) throws IOException {
+        if(radioButtonClient.isSelected()) {
+            ViewSwitcher.switchScene(event, "client-view", "client-style", 800, 600,this);
+        } else if(radioButtonEmployee.isSelected()) {
+            ViewSwitcher.switchScene(event, "sorter-view", "employee-style", 800, 600,this);
+        } else if(radioButtonAdmin.isSelected()) {
 
-    }
-
-    private boolean checkLoginValidity() {
-       return false;
+        }
     }
 
     public void switchToRegisterScene(ActionEvent event) throws IOException {
-
+        ViewSwitcher.switchScene(event, "register-view", "login-style",800, 800, this);
     }
 }
