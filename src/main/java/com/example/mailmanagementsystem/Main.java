785f18613e65c9d29816d29ff5fa79a8b57717af
Bartłomiej Krukowski <bartekkrukowski822@gmail.com>
2024-01-10 12:48:10 +0100

    add the builder pattern to the class Package

--- src/main/java/com/example/mailmanagementsystem/Main.java
+++ src/main/java/com/example/mailmanagementsystem/Main.java
@@ -3,12 +3,17 @@ package com.example.mailmanagementsystem;
 import com.example.mailmanagementsystem.models.Address;
 import com.example.mailmanagementsystem.models.Client;
 import com.example.mailmanagementsystem.models.Letter;
+import com.example.mailmanagementsystem.models.Package;
 
 public class Main {
     public static void main(String[] args) {
-        // Test wzorca budowniczy w klasie Letter
         Client sender = new Client("Jan", "Kowalski", "500100500", new Address("Polska", "Kielce", "20-200", "Warszawska", "2", "2"), 1);
         Client recipient = new Client("Dorota", "Nowak", "500100400", new Address("Polska", "Warszawa", "20-300", "Swietokrzyska", "2", "2"), 1);
-        Letter.Builder letterbuilder = new Letter.Builder().parcelID(1).sender(sender).recipient(recipient).destinationAddress(recipient.getAddress()).status("sent").format("A4").isPriority(true);
+
+        // Test wzorca budowniczy w klasie Letter
+        Letter letter = new Letter.Builder().parcelID(1).sender(sender).recipient(recipient).destinationAddress(recipient.getAddress()).status("sent").format("A4").isPriority(true).build();
+
+        // Test wzoraca budowniczy w klasie Package
+        Package package1 = new Package.Builder().parcelID(1).sender(sender).recipient(recipient).destinationAddress(recipient.getAddress()).status("sent").weight(10.1).height(10.2).width(12.2).length(5.5).isInsured(true).build();
     }
 }
--- src/main/java/com/example/mailmanagementsystem/models/Package.java
+++ src/main/java/com/example/mailmanagementsystem/models/Package.java
@@ -7,7 +7,7 @@ public class Package extends Parcel{
     private  double length;
     private boolean isInsured;
 
-    public Package(int parcelID, Client sender, Client recipient, Address destinationAddress, String status, double weight, double height, double width, double length, boolean isInsured) {
+    private Package(int parcelID, Client sender, Client recipient, Address destinationAddress, String status, double weight, double height, double width, double length, boolean isInsured) {
         super(parcelID, sender, recipient, destinationAddress, status);
         this.weight = weight;
         this.height = height;
@@ -55,4 +55,72 @@ public class Package extends Parcel{
     public void setInsured(boolean insured) {
         isInsured = insured;
     }
+
+    public static class Builder {
+        private int parcelID;
+        private Client sender;
+        private Client recipient;
+        private Address destinationAddress;
+        private String status;
+        private double weight = 1.0;
+        private double height = 1.0;
+        private double width = 1.0;
+        private double length = 1.0;
+        private boolean isInsured = false;
+
+        public Package build() {
+            return new Package(parcelID, sender, recipient, destinationAddress, status, weight, height, width, length, isInsured);
+        }
+
+        public Builder parcelID(int parcelID) {
+            this.parcelID = parcelID;
+            return this;
+        }
+
+        public Builder sender(Client sender) {
+            this.sender = sender;
+            return this;
+        }
+
+        public Builder recipient(Client recipient) {
+            this.recipient = recipient;
+            return this;
+        }
+
+        public Builder destinationAddress(Address destinationAddress) {
+            this.destinationAddress = destinationAddress;
+            return this;
+        }
+
+        public Builder status(String status) {
+            this.status = status;
+            return this;
+        }
+
+        public Builder weight(double weight) {
+            this.weight = weight;
+            return this;
+        }
+
+        public Builder height(double height) {
+            this.height = height;
+            return this;
+        }
+
+        public Builder width(double width) {
+            this.width = width;
+            return this;
+        }
+
+        public Builder length(double length) {
+            this.length = length;
+            return this;
+        }
+
+        public Builder isInsured(boolean isInsured) {
+            this.isInsured = isInsured;
+            return this;
+        }
+    }
+
 }
