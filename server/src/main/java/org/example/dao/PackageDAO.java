d05f603da5d84548406b4f9727a858b8f980f4e3
Bartek <bartekkrukowski822@gmail.com>
2024-01-24 20:25:51 +0100

    refactor sendPackageInfo to use PackageDAO

--- server/src/main/java/org/example/dao/PackageDAO.java
+++ server/src/main/java/org/example/dao/PackageDAO.java
@@ -1,7 +1,5 @@
 package org.example.dao;
 
-import org.example.models.Address;
-import org.example.models.Client;
 import org.example.models.Package;
 import org.example.models.Parcel;
 import org.example.utils.DatabaseConnector;
@@ -9,6 +7,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PackageDAO {
 
@@ -55,5 +55,55 @@ public class PackageDAO {
         }
 
         return null;
+    }
+
+    public List<String> getPackageInfoByUserId(String userID) throws SQLException {
+        List<String> packageInfoList = new ArrayList<>();
+        String query = "SELECT " +
+                "p.parcel_id AS \"Parcel Number\", " +
+                "p.status AS \"Status\", " +
+                "p.send_date AS \"Sending Date\", " +
+                "p.estimated_delivery_date AS \"Estimated Delivery Date\", " +
+                "p.is_express_delivery AS \"Express Delivery\", " +
+                "p.is_fragile AS \"Fragile\", " +
+                "p.insured_value AS \"Insurance Value\", " +
+                "s.first_name || ' ' || s.last_name AS \"Sender\", " +
+                "r.first_name || ' ' || r.last_name AS \"Recipient\", " +
+                "pkg.weight AS \"Weight\", " +
+                "pkg.height || 'x' || pkg.width || 'x' || pkg.length AS \"Dimensions\", " +
+                "pkg.info AS \"Information\" " +
+                "FROM Parcels p " +
+                "JOIN Clients sender ON p.sender_id = sender.client_id " +
+                "JOIN Persons s ON sender.person_id = s.person_id " +
+                "JOIN Clients recipient ON p.recipient_id = recipient.client_id " +
+                "JOIN Persons r ON recipient.person_id = r.person_id " +
+                "JOIN Packages pkg ON p.parcel_id = pkg.parcel_id " +
+                "JOIN UserLogins ul ON s.person_id = ul.person_id " +
+                "WHERE ul.login_id = ?";
+
+        try (Connection connection = DatabaseConnector.getConnection();
+             PreparedStatement stmt = connection.prepareStatement(query)) {
+
+            stmt.setString(1, userID);
+            try (ResultSet rs = stmt.executeQuery()) {
+                while (rs.next()) {
+                    StringBuilder packageInfo = new StringBuilder();
+                    packageInfo.append(rs.getString("Parcel Number")).append(",");
+                    packageInfo.append(rs.getString("Status")).append(",");
+                    packageInfo.append(rs.getString("Sending Date")).append(",");
+                    packageInfo.append(rs.getString("Estimated Delivery Date")).append(",");
+                    packageInfo.append(rs.getInt("Express Delivery") == 1 ? "Yes" : "No").append(",");
+                    packageInfo.append(rs.getInt("Fragile") == 1 ? "Yes" : "No").append(",");
+                    packageInfo.append(rs.getDouble("Insurance Value")).append(",");
+                    packageInfo.append(rs.getString("Sender")).append(",");
+                    packageInfo.append(rs.getString("Recipient")).append(",");
+                    packageInfo.append(rs.getDouble("Weight")).append(",");
+                    packageInfo.append(rs.getString("Dimensions")).append(",");
+                    packageInfo.append(rs.getString("Information")).append("|");
+                    packageInfoList.add(packageInfo.toString());
+                }
+            }
+        }
+        return packageInfoList;
     }
 }
--- server/src/main/java/org/example/services/PackageService.java
+++ server/src/main/java/org/example/services/PackageService.java
@@ -1,59 +1,25 @@
 package org.example.services;
 
+import org.example.dao.PackageDAO;
 import org.example.utils.DatabaseConnector;
 
 import java.io.PrintWriter;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 
 public class PackageService {
     public void sendPackageInfo(String userID, PrintWriter clientOut) {
-        try (PreparedStatement stmt = DatabaseConnector.getConnection().prepareStatement("SELECT\n" +
-                "    p.parcel_id AS \"Parcel Number\",\n" +
-                "    p.status AS \"Status\",\n" +
-                "    p.send_date AS \"Sending Date\",\n" +
-                "    p.estimated_delivery_date AS \"Estimated Delivery Date\",\n" +
-                "    p.is_express_delivery AS \"Express Delivery\",\n" +
-                "    p.is_fragile AS \"Fragile\",\n" +
-                "    p.insured_value AS \"Insurance Value\",\n" +
-                "    s.first_name || ' ' || s.last_name AS \"Sender\",\n" +
-                "    r.first_name || ' ' || r.last_name AS \"Recipient\",\n" +
-                "    pkg.weight AS \"Weight\",\n" +
-                "    pkg.height || 'x' || pkg.width || 'x' || pkg.length AS \"Dimensions\",\n" +
-                "    pkg.info AS \"Information\"\n" +
-                "FROM\n" +
-                "    Parcels p\n" +
-                "    JOIN Clients sender ON p.sender_id = sender.client_id\n" +
-                "    JOIN Persons s ON sender.person_id = s.person_id\n" +
-                "    JOIN Clients recipient ON p.recipient_id = recipient.client_id\n" +
-                "    JOIN Persons r ON recipient.person_id = r.person_id\n" +
-                "    JOIN Packages pkg ON p.parcel_id = pkg.parcel_id\n" +
-                "    JOIN UserLogins ul ON s.person_id = ul.person_id -- Dodane połączenie z UserLogins\n" +
-                "WHERE\n" +
-                "    ul.login_id = ?")) {
-            stmt.setString(1, userID);
-            ResultSet rs = stmt.executeQuery();
+        PackageDAO packageDAO = new PackageDAO();
+        try {
+            List<String> packageInfoList = packageDAO.getPackageInfoByUserId(userID);
             StringBuilder packageInfo = new StringBuilder();
-
-            while (rs.next()) {
-                packageInfo.append(rs.getString("Parcel Number")).append(",");
-                packageInfo.append(rs.getString("Status")).append(",");
-                packageInfo.append(rs.getString("Sending Date")).append(",");
-                packageInfo.append(rs.getString("Estimated Delivery Date")).append(",");
-                packageInfo.append(rs.getString("Express Delivery")).append(",");
-                packageInfo.append(rs.getString("Fragile")).append(",");
-                packageInfo.append(rs.getString("Insurance Value")).append(",");
-                packageInfo.append(rs.getString("Sender")).append(",");
-                packageInfo.append(rs.getString("Recipient")).append(",");
-                packageInfo.append(rs.getString("Weight")).append(",");
-                packageInfo.append(rs.getString("Dimensions")).append(",");
-                packageInfo.append(rs.getString("Information")).append("|");
+            for (String info : packageInfoList) {
+                packageInfo.append(info);
+                System.out.println(packageInfo);
             }
-
-            System.out.println(packageInfo.toString());
-            clientOut.println(packageInfo.toString());
-
+            clientOut.println(packageInfo);
         } catch (SQLException e) {
             clientOut.println("ERROR");
             e.printStackTrace();
